5de306a3e9eb63834eab9c41032891fc0eab6060
ahersi206 <50468179+ahersi206@users.noreply.github.com>
2019-12-20 16:34:25 -0800

    Update MyOI.java

--- src/frc/team8051/MyOI.java
+++ src/frc/team8051/MyOI.java
@@ -4,15 +4,25 @@ package frc.team8051;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class MyOI {
-    public Joystick rightJoystick = new Joystick(0);
-    public Joystick leftJoystick = new Joystick(1);
+    //public Joystick rightJoystick = new Joystick(0);
+    //public Joystick leftJoystick = new Joystick(1);
+
+    public Joystick joystick = new Joystick(1);
 
     public double getRightJoystick() {
-        return rightJoystick.getY();
+        return joystick.getRawAxis(1);
     }
 
-    public double getLeftJoystick() {
-        return leftJoystick.getY();
+    public double getLeftJoystick(){
+        return joystick.getRawAxis(5);
     }
+
+    //public double getRightJoystick() {
+        //return rightJoystick.getY();
+   // }
+
+    //public double getLeftJoystick() {
+      //  return leftJoystick.getY();
+    //}
 
 }
