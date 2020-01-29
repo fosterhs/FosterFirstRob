a7683fc0aca66b7b91a07fafd51081b20c42acd5
2khuu <mry2khuu@gmail.com>
2020-01-28 16:59:58 -0800

    differentialdrivebase

--- src/frc/team8051/subsystems/DifferentialDriveBase.java
+++ src/frc/team8051/subsystems/DifferentialDriveBase.java
@@ -1,5 +1,24 @@
 package frc.team8051.subsystems;
+import edu.wpi.first.wpilibj.SendableBase;
+import edu.wpi.first.wpilibj.drive.RobotDriveBase;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+private VictorSPX rightMotor;
+private VictorSPX leftMotor;
+public class DifferentialDriveBase extends DifferntialDrive {
 
-public class DifferentialDriveBase {
-    
+    public static final double kDefaultQuickStopAlpha;
+
+    public static final double kDefaultQuickStopAlpha;
+
+
+    public DifferentialDriveBase(SuperController rightmotor, SuperController leftmotor){
+        rightMotor = new VictorSPX(14);
+        leftMotor = new VictorSPX(15);
+    }
+
+    public void arcadeDrive(double xSpeed, double zRotation){
+            xSpeed = 1.0;
+            zRotation = 1.0;
+    }
+
 }
